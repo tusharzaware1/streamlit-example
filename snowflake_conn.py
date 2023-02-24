c96549816897b6fc7c105090b2ef29c24f9a44e6
tusharzaware1 <82178993+tusharzaware1@users.noreply.github.com>
2023-02-24 23:37:22 +0000

    Update snowflake_conn.py

--- snowflake_conn.py
+++ snowflake_conn.py
@@ -1,5 +1,3 @@
-# streamlit_app.py
-
 import streamlit as st
 import snowflake.connector
 
@@ -7,9 +5,14 @@ import snowflake.connector
 # Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
-    return snowflake.connector.connect(
-        **st.secrets["snowflake"], client_session_keep_alive=True
-    )
+    try:
+        conn = snowflake.connector.connect(
+            **st.secrets["snowflake"], client_session_keep_alive=True
+        )
+        return conn
+    except Exception as e:
+        st.error(f"Error connecting to Snowflake: {e}")
+        return None
 
 conn = init_connection()
 
@@ -18,11 +21,19 @@ conn = init_connection()
 @st.cache_data(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+        try:
+            cur.execute(query)
+            return cur.fetchall()
+        except Exception as e:
+            st.error(f"Error running query: {e}")
+            return None
 
+# Create placeholder for results.
+results_placeholder = st.empty()
+
+# Get results.
 rows = run_query("SELECT * from BQ_TO_SF.BQ_TO_SF_MIG.MIGRATION_LOG;")
 
-# Print results.
-for row in rows:
-    st.write(row)
+# Display results.
+if rows is not None:
+    results_placeholder.table(rows)
