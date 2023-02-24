522b888002700cd22725a6da967b79466da51bf6
tusharzaware1 <82178993+tusharzaware1@users.noreply.github.com>
2023-02-24 18:38:17 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
-# from collections import namedtuple
-# import altair as alt
-# import math
-# import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 st.write('Welcome Tushar')
